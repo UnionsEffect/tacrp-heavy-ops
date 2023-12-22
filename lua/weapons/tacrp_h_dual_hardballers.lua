d0ae1af9d7bed13a5095500f4f52da804e634ee8
Fesiug <publicfesiug@outlook.com>
2023-12-22 14:44:07 -0500

    Dual Hardballers
    uses dual 1911 stats and doesn't have WM

---
+++ lua/weapons/tacrp_h_dual_hardballers.lua
@@ -0,0 +1,309 @@
+SWEP.Base = "tacrp_base"
+SWEP.Spawnable = true
+
+AddCSLuaFile()
+
+// names and stuff
+SWEP.PrintName = "Dual Hardballers"
+SWEP.Category = "Tactical RP"
+
+SWEP.SubCatTier = "2Operator"
+SWEP.SubCatType = "1Sidearm"
+
+SWEP.Description = "Lorem ipsum."
+SWEP.Description_Quote = "Hitman quote"
+
+SWEP.Trivia_Caliber = ".45 ACP"
+SWEP.Trivia_Manufacturer = "Arcadia Machine & Tool."
+SWEP.Trivia_Year = "1980"
+
+SWEP.Faction = TacRP.FACTION_NEUTRAL
+SWEP.Credits = [[
+Assets: Terminator: Resistance (originally ported by Sirgibsalot)
+Sounds: Navaro
+Animation: Tactical Intervention, Fesiug
+]]
+
+
+SWEP.ViewModel = "models/weapons/tacint_shark/v_dual_hardballers.mdl"
+SWEP.WorldModel = "models/weapons/tacint_shark/w_dual_1911.mdl"
+
+SWEP.Slot = 1
+
+SWEP.BalanceStats = {
+    [TacRP.BALANCE_SBOX] = {
+        Damage_Max = 37,
+        Damage_Min = 10,
+
+        BodyDamageMultipliers = {
+            [HITGROUP_HEAD] = 3.75,
+            [HITGROUP_CHEST] = 1,
+            [HITGROUP_STOMACH] = 1,
+            [HITGROUP_LEFTARM] = 0.9,
+            [HITGROUP_RIGHTARM] = 0.9,
+            [HITGROUP_LEFTLEG] = 0.75,
+            [HITGROUP_RIGHTLEG] = 0.75,
+            [HITGROUP_GEAR] = 0.9
+        },
+
+        MoveSpeedMult = 0.95,
+        ShootingSpeedMult = 0.8,
+        SightedSpeedMult = 1,
+        MeleeSpeedMult = 1,
+        ReloadSpeedMult = 1,
+    },
+    [TacRP.BALANCE_TTT] = {
+        Damage_Max = 27,
+        Damage_Min = 8,
+        Range_Min = 300,
+        Range_Max = 1600,
+        RPM = 360,
+
+        BodyDamageMultipliers = {
+            [HITGROUP_HEAD] = 2.5,
+            [HITGROUP_CHEST] = 1,
+            [HITGROUP_STOMACH] = 1,
+            [HITGROUP_LEFTARM] = 0.9,
+            [HITGROUP_RIGHTARM] = 0.9,
+            [HITGROUP_LEFTLEG] = 0.75,
+            [HITGROUP_RIGHTLEG] = 0.75,
+            [HITGROUP_GEAR] = 0.9
+        },
+
+        ReloadTimeMult = 1.25,
+
+        MoveSpeedMult = 1,
+        ShootingSpeedMult = 0.9,
+        MeleeSpeedMult = 1,
+        SightedSpeedMult = 0.75,
+        ReloadSpeedMult = 0.9,
+    },
+    [TacRP.BALANCE_PVE] = {
+        Damage_Max = 22,
+        Damage_Min = 11,
+
+        Spread = 0.008,
+        RecoilKick = 6,
+
+        MoveSpeedMult = 0.95,
+        ShootingSpeedMult = 0.8,
+        SightedSpeedMult = 1,
+        MeleeSpeedMult = 1,
+        ReloadSpeedMult = 1,
+    },
+}
+
+SWEP.TTTReplace = {["weapon_zm_pistol"] = 0.5, ["weapon_ttt_glock"] = 0.5}
+
+// "ballistics"
+
+SWEP.Damage_Max = 34
+SWEP.Damage_Min = 8
+SWEP.Range_Min = 400
+SWEP.Range_Max = 1800
+SWEP.Penetration = 4
+SWEP.ArmorPenetration = 0.75
+
+SWEP.MuzzleVelocity = 9500
+
+SWEP.BodyDamageMultipliers = {
+    [HITGROUP_HEAD] = 4.5,
+    [HITGROUP_CHEST] = 1,
+    [HITGROUP_STOMACH] = 1,
+    [HITGROUP_LEFTARM] = 0.9,
+    [HITGROUP_RIGHTARM] = 0.9,
+    [HITGROUP_LEFTLEG] = 0.75,
+    [HITGROUP_RIGHTLEG] = 0.75,
+    [HITGROUP_GEAR] = 0.9
+}
+
+// misc. shooting
+
+SWEP.Firemode = 1
+
+SWEP.RPM = 450
+
+SWEP.Spread = 0.015
+
+SWEP.MoveSpreadPenalty = 0
+SWEP.MidAirSpreadPenalty = 0.075
+SWEP.HipFireSpreadPenalty = 0
+
+SWEP.RecoilPerShot = 1
+SWEP.RecoilMaximum = 8
+SWEP.RecoilResetTime = 0.14
+SWEP.RecoilDissipationRate = 12
+SWEP.RecoilFirstShotMult = 1 // multiplier for the first shot's recoil amount
+
+SWEP.RecoilVisualKick = 1
+SWEP.RecoilKick = 8
+SWEP.RecoilStability = 0.15
+
+SWEP.RecoilSpreadPenalty = 0.004
+
+SWEP.Sway = 0.5
+
+SWEP.CanBlindFire = false
+SWEP.CanSuicide = true
+
+SWEP.ShootTimeMult = 0.6
+
+// handling
+
+SWEP.MoveSpeedMult = 0.95
+SWEP.ShootingSpeedMult = 0.85
+SWEP.SightedSpeedMult = 1
+
+SWEP.ReloadSpeedMult = 0.6
+
+SWEP.AimDownSightsTime = 0.25
+SWEP.SprintToFireTime = 0.25
+
+// hold types
+
+SWEP.HoldType = "duel"
+SWEP.HoldTypeSprint = "normal"
+SWEP.HoldTypeBlindFire = false
+SWEP.HoldTypeSuicide = "duel"
+
+SWEP.GestureShoot = ACT_HL2MP_GESTURE_RANGE_ATTACK_DUEL
+SWEP.GestureReload = ACT_HL2MP_GESTURE_RELOAD_DUEL
+
+SWEP.PassiveAng = Angle(5, 0, -2)
+SWEP.PassivePos = Vector(3, 0, -6)
+
+SWEP.CustomizeAng = Angle(0, 35, 0)
+SWEP.CustomizePos = Vector(1, 0, -12)
+
+SWEP.BlindFireAng = Angle(0, 0, 0)
+SWEP.BlindFirePos = Vector(0, 0, 0)
+
+SWEP.BlindFireSuicideAng = Angle(0, 130, 0)
+SWEP.BlindFireSuicidePos = Vector(1, 22, -15)
+
+SWEP.SprintAng = Angle(0, 30, 0)
+SWEP.SprintPos = Vector(1, 0, -12)
+
+SWEP.HolsterVisible = true
+SWEP.HolsterSlot = TacRP.HOLSTER_SLOT_PISTOL
+SWEP.HolsterPos = Vector(0, 3, -4)
+SWEP.HolsterAng = Angle(60, 5, 0)
+
+// reload
+
+SWEP.ClipSize = 16
+SWEP.Ammo = "pistol"
+
+SWEP.ReloadTimeMult = 1
+
+SWEP.DropMagazineModel = "models/weapons/tacint_shark/magazines/dual_1911.mdl"
+SWEP.DropMagazineImpact = "pistol"
+SWEP.DropMagazineAmount = 2
+
+SWEP.ReloadUpInTime = 2.8
+SWEP.DropMagazineTime = 0.25
+
+// sounds
+
+local path = "TacRP/weapons/gsr1911/gsr1911_"
+local path1 = "Tacint_shark/hardballer/"
+
+SWEP.Sound_Shoot = "^" .. path1 .. "hardballer_fire.wav"
+SWEP.Sound_Shoot_Silenced = path1 .. "shots.wav"
+
+SWEP.Vol_Shoot = 110
+SWEP.ShootPitchVariance = 2.5 // amount to vary pitch by each shot
+
+// effects
+
+// the .qc attachment for the muzzle
+SWEP.QCA_Muzzle = 4
+
+SWEP.WM_QCA_MuzzleL = 1
+SWEP.WM_QCA_MuzzleR = 2
+SWEP.WM_QCA_EjectL = 3
+SWEP.WM_QCA_EjectR = 4
+
+SWEP.MuzzleEffect = "muzzleflash_pistol"
+
+// anims
+
+SWEP.AnimationTranslationTable = {
+    ["deploy"] = "draw",
+    ["blind_idle"] = "idle",
+    ["blind_fire"] = "idle",
+    ["melee"] = {"melee1", "melee2"},
+    ["shoot_left"] = {"shoot_left-1", "shoot_left-2"},
+    ["shoot_right"] = {"shoot_right-1", "shoot_right-2"}
+}
+
+SWEP.LastShot = true
+SWEP.Akimbo = true
+SWEP.EffectsAlternate = true
+
+// attachments
+
+SWEP.Attachments = {
+    [1] = {
+        PrintName = "Tactical",
+        Category = "tactical",
+        Bone = "ValveBiped.mtx_root2",
+        WMBone = "Box01",
+        AttachSound = "TacRP/weapons/flashlight_on.wav",
+        DetachSound = "TacRP/weapons/flashlight_off.wav",
+        VMScale = 0.9,
+        WMScale = 1,
+        Pos_VM = Vector(-2, -0.2, 5.45),
+        Ang_VM = Angle(90, 0, 180),
+        Pos_WM = Vector(-0.1, 8, -1.7),
+        Ang_WM = Angle(0, -90, 180),
+    },
+    [2] = {
+        PrintName = "Accessory",
+        Category = {"acc_dual", "acc_extmag_dual2"},
+        AttachSound = "TacRP/weapons/flashlight_on.wav",
+        DetachSound = "TacRP/weapons/flashlight_off.wav",
+    },
+    [4] = {
+        PrintName = "Perk",
+        Category = {"perk", "perk_melee", "perk_shooting", "perk_reload"},
+        AttachSound = "tacrp/weapons/flashlight_on.wav",
+        DetachSound = "tacrp/weapons/flashlight_off.wav",
+    },
+    [2] = {
+        PrintName = "Trigger",
+        Category = {"trigger_akimbo"},
+        AttachSound = "TacRP/weapons/flashlight_on.wav",
+        DetachSound = "TacRP/weapons/flashlight_off.wav",
+    },
+    [3] = {
+        PrintName = "Ammo",
+        Category = {"ammo_pistol"},
+        AttachSound = "TacRP/weapons/flashlight_on.wav",
+        DetachSound = "TacRP/weapons/flashlight_off.wav",
+    }
+}
+
+SWEP.AttachmentCapacity = 30 // amount of "Capacity" this gun can accept
+
+local function addsound(name, spath)
+    sound.Add({
+        name = name,
+        channel = 16,
+        volume = 1.0,
+        sound = spath
+    })
+end
+
+addsound("tacint_dualhardballer.clip_in_left", path1 .. "dualclipin.ogg")
+addsound("tacint_dualhardballer.clip_in_right", path1 .. "clipin.wav")
+addsound("tacint_dualhardballer.clip_out", path1 .. "clipout.wav")
+addsound("tacint_dualhardballer.clipsmack", path1 .. "dualsmack.ogg")
+addsound("tacint_dualhardballer.slide_shut", path1 .. "dualshut.ogg")
+addsound("tacint_dualhardballer.Slide_Back", path1 .. "SlideBack.wav")
+addsound("tacint_dualhardballer.slide_release", path1 .. "sliderelease.wav")
+
+
+SWEP.Scope = false
+
+SWEP.FreeAim = false
