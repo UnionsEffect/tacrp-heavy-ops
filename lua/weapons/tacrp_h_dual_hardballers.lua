4f1b4cb72d57ee7c40c7e347b99022561ffcb11a
Fesiug <publicfesiug@outlook.com>
2023-12-22 17:37:45 -0500

    'Silver'ballers

--- lua/weapons/tacrp_h_dual_hardballers.lua
+++ lua/weapons/tacrp_h_dual_hardballers.lua
@@ -4,7 +4,7 @@ SWEP.Spawnable = true
 AddCSLuaFile()
 
 // names and stuff
-SWEP.PrintName = "Dual Hardballers"
+SWEP.PrintName = "Dual Silverballers"
 SWEP.Category = "Tactical RP"
 
 SWEP.SubCatTier = "2Operator"
